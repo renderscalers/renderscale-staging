2b5aa147fe80812113edf1a9be6287149cd9bd29
renderscalers <smrithi@renderscalers.com>
2026-05-18 23:19:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,9 @@
 //   },
 // });
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   base: '/renderscale-staging/',
 })
